0411fb9c04f6c3d218131709a80b09867bc2bfa7
jacquelinechia <jchia069@gmail.com>
2022-06-28 00:06:05 +0800

    Update file read write

--- src/FileReadWrite.java
+++ src/FileReadWrite.java
@@ -1,23 +1,62 @@
+
+import java.io.*;
+import java.util.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+
 public class FileReadWrite {
 
-    
-    
-    public static ApplicantAnswer[] readExamAnsTxt (){
-       ApplicantAnswer[] appAns = {};
-       return appAns;
+    private static File myf = new File("./data", "applicants.txt");
+
+    public static ApplicantAnswer[] readExamAnsTxt() {
+
+        ApplicantAnswer[] appAns = {};
+        return appAns;
+    }
+
+    public static void writeExamAnsTxt(ApplicantAnswer ans) {
+
+    }
+
+    public static LinkedList<Applicant> readAppTxt() {
+
+        LinkedList<Applicant> app = new LinkedList<Applicant>();
+        Scanner fileScanner , lineScanner;
+        String username;
+        String password;
+        int totApplicants;
+        
+        
+        try {
+            fileScanner = new Scanner(myf);
+            String numOfLines = fileScanner.nextLine();
+            totApplicants = Integer.parseInt(numOfLines); 
+            for(int k = 0; k < totApplicants; k++){
+              String first = fileScanner.nextLine();
+              System.out.println(first);
+              lineScanner = new Scanner(first);
+              lineScanner.useDelimiter(":");
+              username = lineScanner.next();
+              password = lineScanner.next();
+              Applicant ap1 = new Applicant(username, password);
+              app.add(ap1);
+            }
+     
+        } catch (FileNotFoundException e) {
+
+        }
+
+        return app;
+    }
+
+    public static Question[] readQuesAnsTxt() { //basically hwo to print out the exam paper 
+        Question[] quesAns = {};
+        return quesAns;
     }
     
-    public static void writeExamAnsTxt (ApplicantAnswer ans){
-       
-    }    
-    
-    public static Applicant[] readAppTxt (){
-       Applicant[] app = {};
-       return app;
-    }
-    
-    public static Question[] readQuesAnsTxt (){ //basically hwo to print out the exam paper 
-       Question [] quesAns = {};
-       return quesAns ;
-    }
 }
