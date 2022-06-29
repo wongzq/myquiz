5122f210749ec246692f16e4e166bf60a47c95e5
umeemu <leetzenze@gmail.com>
2022-06-30 04:29:57 +0800

    Update and changes to Result

--- src/ResultForm.java
+++ src/ResultForm.java
@@ -1,24 +1,45 @@
+
 import java.util.LinkedList;
 import javafx.collections.FXCollections;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class ResultForm extends Stage {
 
+    private char correctAnswers[] = {'B', 'D', 'A', 'B', 'C', 'B', 'A', 'C', 'A',
+        'C', 'D', 'A', 'B', 'D', 'A', 'C', 'C', 'B', 'D', 'A'};
+    private LinkedList<ApplicantAnswer> applicantAnswers = new LinkedList<ApplicantAnswer>();
+
     private LinkedList<Applicant> applicants;
     private ComboBox<Applicant> cmbname;
-    
+
     private ApplicantAnswer[] answers;
 
     private NavigateToForm toMyQuiz;
     private NavigateToForm toAnalysisForm;
-    
+
+    private Label labResult = new Label("Result: ");
+    private Label labAllAnswer = new Label("");
+    private Label labStuAnswer = new Label("");
+    private Button btnPass = new Button();
+
     public ResultForm() {
+
+        applicantAnswers.add(new ApplicantAnswer(
+                new char[]{'B', 'C', 'B', 'D', 'A', 'B', 'A', 'C', 'A',
+                    'C', 'D', 'A', 'B', 'D', 'A', 'C', 'C', 'B', 'D', 'A'},
+                new ApplicantDetails("John Wong",
+                        "abc123",
+                        20,
+                        "Male",
+                        "Malaysian",
+                        "Class 1",
+                        "123",
+                        "Computer Science")));
 
         // ApplicantName
         applicants = FileReadWrite.readAppTxt();
@@ -32,37 +53,26 @@ public class ResultForm extends Stage {
         cmbname.setLayoutY(50);
         cmbname.setMinWidth(150);
         cmbname.setMaxWidth(150);
+        cmbname.setOnAction(e -> {
+            reloadPage();
+        });
 
         // Result
-        Label labResult = new Label("Result: ");
         labResult.setLayoutX(25); // without offset
         labResult.setLayoutY(100); // without offset
 
-        TextField txtResult = new TextField();
-        txtResult.setLayoutX(150);
-        txtResult.setLayoutY(100);
-
-        //Pass
-        Button btnPass = new Button();
+        //Pass or Fail
         btnPass.setLayoutX(450);
         btnPass.setLayoutY(25);
-        btnPass.setText("Pass");
-        
-        //Fail
-        Button btnFail = new Button();
-        btnFail.setLayoutX(500);
-        btnFail.setLayoutY(25);
-        btnFail.setText("Fail");
 
-        //Answer
-        Label labAnswer = new Label("Answers: ");
-        labAnswer.setLayoutX(25); // without offset
-        labAnswer.setLayoutY(200); // without offset
-
+        //correct answer: B, C, B, D, A, B, A, C, A, C, D, A, B, D, A, C, C, B, D, A
         //AllAnswer
-        Label labAllAnswer = new Label("B, C, B, D, A, B, A, C, A, C, D, A, B, D, A, C, C, B, D, A");
-        labAllAnswer.setLayoutX(100); // without offset
+        labAllAnswer.setLayoutX(25); // without offset
         labAllAnswer.setLayoutY(200); // without offset
+
+        //Student Answer
+        labStuAnswer.setLayoutX(25); // without offset
+        labStuAnswer.setLayoutY(250);
 
         //Next
         Button btnNext = new Button();
@@ -72,7 +82,7 @@ public class ResultForm extends Stage {
         btnNext.setOnAction(e -> {
             toAnalysisForm.navigate();
         });
-        
+
         //Back
         Button btnBack = new Button();
         btnBack.setLayoutX(150);
@@ -81,17 +91,14 @@ public class ResultForm extends Stage {
         btnBack.setOnAction(e -> {
             toMyQuiz.navigate();
         });
-        
 
         Pane p1 = new Pane();
         p1.getChildren().add(labName);
         p1.getChildren().add(cmbname);
         p1.getChildren().add(labResult);
-        p1.getChildren().add(txtResult);
         p1.getChildren().add(btnPass);
-        p1.getChildren().add(btnFail);
-        p1.getChildren().add(labAnswer);
         p1.getChildren().add(labAllAnswer);
+        p1.getChildren().add(labStuAnswer);
         p1.getChildren().add(btnNext);
         p1.getChildren().add(btnBack);
 
@@ -99,22 +106,72 @@ public class ResultForm extends Stage {
         this.setTitle("Result Form");
         this.setScene(myScene);
     }
-    
+
     public void setToMyQuiz(NavigateToForm toMyQuiz) {
         this.toMyQuiz = toMyQuiz;
     }
-    
+
     public void setToAnalysisForm(NavigateToForm toAnalysisForm) {
         this.toAnalysisForm = toAnalysisForm;
     }
+
+    private void reloadPage() {
+        
+        int i = getSelectedIndex();
+        if (i == -1) {
+            labStuAnswer.setText("");
+            btnPass.setText("-");
+            labResult.setText("");
+            labAllAnswer.setText("");
+        } else {
+            double result = getResult();
+            labResult.setText("Result: " + result + "%");
+            labAllAnswer.setText("Correct Answer: " + getArray(correctAnswers));
+            labStuAnswer.setText("Student's Answer: " + getArray(applicantAnswers.get(i).getAnswers()));
+            if (result < 70) {
+                btnPass.setText("Fail");
+            } else {
+                btnPass.setText("Pass");
+            }
+        }
+    }
+
+    private double getResult() {
+        int individualScore = 0;
+        int i = getSelectedIndex();
+        char ans[] = applicantAnswers.get(i).getAnswers();
+        for (int j = 0; j < correctAnswers.length; j++) {
+            if (ans[j] == correctAnswers[j]) {
+                individualScore++;
+            }
+        }
+        double finalScore = (individualScore / 20f) * 100f;
+        return finalScore;
+    }
+
+    private int getSelectedIndex() {
+        for (int i = 0; i < applicantAnswers.size(); i++) {
+            String selectedName = cmbname.getValue().getName();
+            String applicantName = applicantAnswers.get(i).getApplicant().getName();
+            if (selectedName.equals(applicantName)) {
+                return i;
+
+            }
+        }
+        return -1;
+    }
+
+    private String getArray(char[] answers) {
+        String readAnswers = "";
+        for (int i = 0; i < answers.length; i++) {
+            readAnswers = readAnswers + String.valueOf(answers[i]) + " , ";
+
+        }
+        return readAnswers;
+    }
 }
 
-
 //private void selectStudent() {
-//
-//}
-//
-//private void calcScore() {
 //
 //}
 //
